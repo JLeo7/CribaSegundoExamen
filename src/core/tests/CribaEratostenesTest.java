cc2d2acdfea28fb6e5636f5e8ea30b5e92662794
JLeo7 <jleomoram@gmail.com>
2018-07-26 12:31:29 -0600

    Arreglo del segundo test

--- src/core/tests/CribaEratostenesTest.java
+++ src/core/tests/CribaEratostenesTest.java
@@ -1,26 +1,20 @@
 package core.tests;
 
 import core.CribaEratostenes;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CribaEratostenesTest {
 
-    static int primos[];
-
-    @BeforeClass
-    public static void init(){
-        int  [] primos = CribaEratostenes.generaCriba(10);
-    }
-
     @Test
     public void testCribaGeneradaCorrectamente() {
+        int  [] primos = CribaEratostenes.generaCriba(10);
         assertArrayEquals(new int[]{2,3,5,7},primos);
     }
 
     @Test
     public void testTopeMenorA2() {
+        int  [] primos = CribaEratostenes.generaCriba(1);
         assertArrayEquals(new int[0], primos);
     }
 }
